fbb4f6c45617061afb00cc03397d2ce6b99e7116
SantiFontana <santiagofontana61@gmail.com>
2025-01-07 13:47:46 -0300

    Agregando la validación del input name

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import FormSignUp from './components/FormSignUp';
 
 function App() {
+
+  const handleSubmit = ( valores ) => {
+    console.log("APPJS: ", valores)
+  }
+
   return (
     <Container component="section" maxWidth="sm">
       <Typography variant="h3" align="center" component="h1">Formulario Registro</Typography>
-      <FormSignUp />
+      <FormSignUp handleSubmit = {handleSubmit} />
     </Container>
   );
 }
--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,51 +1,87 @@
 import { useState } from "react"
 import { Button, TextField, Switch, FormGroup, FormControlLabel, FormControl } from "@mui/material"
 
-function FormSignUp() {
+function FormSignUp({ handleSubmit }) {
     const [name, setName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [promo, setPromo] = useState(true)
+    const [nov, setNov] = useState(false)
+
+    const [errors, setErrors] = useState({
+        name: {
+            error: false,
+            message: "Debe tener al menos 3 caracteres"
+        }
+    })
+
+    function validarNombre(name){
+        if(name.length >= 3){
+            return { name: {error: false, message: ""} }
+        } else {
+            return { name: {error: true, message: "Debe tener al menos 3 caracteres"} }
+        }
+    }
 
     return (
-        <form>
+        <form onSubmit={(e) => {
+            e.preventDefault()
+            handleSubmit({
+                name, 
+                lastName, 
+                email, 
+                promo, 
+                nov
+            })
+        }}>
             <TextField 
                 id="name" 
                 label="Nombre" 
                 variant="outlined" 
                 fullWidth 
                 margin="normal"
-                onChange={(e) => {
-                    console.log(e.target.value)
-                    setName(e.target.value)
-                }}
+                onChange={(e) => setName(e.target.value)}
                 value={name}
+                error={ errors.name.error }
+                helperText={errors.name.error ? errors.name.message : ""}
+                onBlur={(e) => {
+                    setErrors(validarNombre(e.target.value))
+                }}
             />
             <TextField 
                 id="latName" 
                 label="Apellidos" 
-                variant="outlined" 
-                color="error" 
+                variant="outlined"  
                 fullWidth 
-                margin="normal" 
+                margin="normal"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
             />
             <TextField 
                 id="email" 
                 label="Email" 
                 variant="outlined" 
                 fullWidth 
-                margin="normal" 
+                margin="normal"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
             />
             <FormGroup>
                 <FormControlLabel 
                     control = {
-                        <Switch defaultChecked/>
+                        <Switch
+                        checked = {promo} 
+                        onChange={(e) => setPromo(e.target.checked)} 
+                        />
                 } 
                     label="Promociones" 
                 />
                 <FormControlLabel
-                    control = {<Switch defaultChecked/>}
+                    control = {<Switch checked= {nov} onChange={(e) => setNov(e.target.checked)} />}
                     label = "Novedades"
                 />
             </FormGroup>
-            <Button variant = "contained">Registrarse</Button>
+            <Button variant = "contained" type="submit">Registrarse</Button>
         </form>
     )
 }
